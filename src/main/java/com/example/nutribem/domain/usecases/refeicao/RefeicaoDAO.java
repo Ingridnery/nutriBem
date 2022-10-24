0ef2fbffa9602daeb6a570bd3d038113d4358c67
IngridNery <neryingrid16@gmail.com>
2022-10-24 20:12:37 -0300

    [FEAT] Cria a interface RefeicaoDAO

---
+++ src/main/java/com/example/nutribem/domain/usecases/refeicao/RefeicaoDAO.java
@@ -0,0 +1,7 @@
+package com.example.nutribem.domain.usecases.refeicao;
+
+import com.example.nutribem.domain.entities.refeicao.Refeicao;
+import com.example.nutribem.domain.usecases.utils.DAO;
+
+public interface RefeicaoDAO extends DAO<Refeicao,Integer> {
+}
