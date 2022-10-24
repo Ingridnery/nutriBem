ebb3f182d95bf9a858aa269540fbc531a3604185
DiegoPagotto <diegodeabreupagotto@gmail.com>
2022-10-24 15:04:29 -0300

    [FEAT] Criadas as classes Validator e Notification

---
+++ src/main/java/com/example/nutribem/domain/usecases/utils/Notification.java
@@ -0,0 +1,40 @@
+package com.example.nutribem.domain.usecases.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Notification {
+
+    private List<Error> errors = new ArrayList<>();
+
+    public void addError(String message, Exception e){
+        errors.add(new Error(message, e));
+    }
+    public void addError(String message){
+        addError(message, null);
+    }
+
+    public boolean isCorrect(){
+        return errors.isEmpty();
+    }
+
+    public boolean hasErrors(){
+        return !isCorrect();
+    }
+
+    private static class Error{
+        String message;
+        Exception cause;
+
+        public Error(String message, Exception cause){
+            this.message = message;
+            this.cause = cause;
+        }
+    }
+
+    public String errorMessage(){
+        return errors.stream().map(e -> e.message).collect(Collectors.joining(" ,"));
+    }
+
+}
