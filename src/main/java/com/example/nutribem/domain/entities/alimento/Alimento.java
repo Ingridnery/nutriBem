e14c42937983fe20815c70d5ac66ef53b2ba46fa
DiegoPagotto <diegodeabreupagotto@gmail.com>
2022-10-24 14:35:45 -0300

    [FEAT] Adicionados Getters e Setters de Alimento

--- src/main/java/com/example/nutribem/domain/entities/alimento/Alimento.java
+++ src/main/java/com/example/nutribem/domain/entities/alimento/Alimento.java
@@ -13,9 +13,9 @@ public class Alimento {
     private Float acucar;
    private Float lactose;
 
-    public Alimento(String nome, Integer id, Integer porcao, Integer calorias, Integer colesterol, Integer gluten, Float gordurasSaturadas, Float sodio, Float acucar, Float lactose) {
+    public Alimento(String nome, Integer porcao, Integer calorias, Integer colesterol, Integer gluten, Float gordurasSaturadas, Float sodio, Float acucar, Float lactose) {
         this.nome = nome;
-        this.id = id;
+        this.id = null;
         this.porcao = porcao;
         this.calorias = calorias;
         this.colesterol = colesterol;
@@ -23,6 +23,86 @@ public class Alimento {
         this.gordurasSaturadas = gordurasSaturadas;
         this.sodio = sodio;
         this.acucar = acucar;
+        this.lactose = lactose;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPorcao() {
+        return porcao;
+    }
+
+    public void setPorcao(Integer porcao) {
+        this.porcao = porcao;
+    }
+
+    public Integer getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(Integer calorias) {
+        this.calorias = calorias;
+    }
+
+    public Integer getColesterol() {
+        return colesterol;
+    }
+
+    public void setColesterol(Integer colesterol) {
+        this.colesterol = colesterol;
+    }
+
+    public Integer getGluten() {
+        return gluten;
+    }
+
+    public void setGluten(Integer gluten) {
+        this.gluten = gluten;
+    }
+
+    public Float getGordurasSaturadas() {
+        return gordurasSaturadas;
+    }
+
+    public void setGordurasSaturadas(Float gordurasSaturadas) {
+        this.gordurasSaturadas = gordurasSaturadas;
+    }
+
+    public Float getSodio() {
+        return sodio;
+    }
+
+    public void setSodio(Float sodio) {
+        this.sodio = sodio;
+    }
+
+    public Float getAcucar() {
+        return acucar;
+    }
+
+    public void setAcucar(Float acucar) {
+        this.acucar = acucar;
+    }
+
+    public Float getLactose() {
+        return lactose;
+    }
+
+    public void setLactose(Float lactose) {
         this.lactose = lactose;
     }
 }
