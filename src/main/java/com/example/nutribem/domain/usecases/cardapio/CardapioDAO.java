fb777e45fc2d1344c6d3a4ed2f2ca755060dd7b7
IngridNery <neryingrid16@gmail.com>
2022-10-24 20:06:54 -0300

    [FEAT] Cria a interface CardapioDAO

---
+++ src/main/java/com/example/nutribem/domain/usecases/cardapio/CardapioDAO.java
@@ -0,0 +1,7 @@
+package com.example.nutribem.domain.usecases.cardapio;
+
+import com.example.nutribem.domain.entities.cardapio.Cardapio;
+import com.example.nutribem.domain.usecases.utils.DAO;
+
+public interface CardapioDAO extends DAO<Cardapio,Integer> {
+}
