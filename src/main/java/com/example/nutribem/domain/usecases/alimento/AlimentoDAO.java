05e1a3062b4a8197990b3978e257b0ebb36df3cd
DiegoPagotto <diegodeabreupagotto@gmail.com>
2022-10-24 15:05:49 -0300

    [FEAT] Criadas a interface AlimentoDAO

---
+++ src/main/java/com/example/nutribem/domain/usecases/alimento/AlimentoDAO.java
@@ -0,0 +1,9 @@
+package com.example.nutribem.domain.usecases.alimento;
+
+import com.example.nutribem.domain.entities.alimento.Alimento;
+import com.example.nutribem.domain.usecases.utils.DAO;
+
+import java.util.Optional;
+
+public interface AlimentoDAO extends DAO<Alimento, Integer> {
+}
