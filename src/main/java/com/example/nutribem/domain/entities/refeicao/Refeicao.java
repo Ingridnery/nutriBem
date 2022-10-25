c776dc58f626f88858eb7d04caa547ec93ae141d
IngridNery <neryingrid16@gmail.com>
2022-10-25 08:19:02 -0300

    [FIX] Altera classe Refeicao

--- src/main/java/com/example/nutribem/domain/entities/refeicao/Refeicao.java
+++ src/main/java/com/example/nutribem/domain/entities/refeicao/Refeicao.java
@@ -7,4 +7,28 @@ public class Refeicao {
     private Integer id;
     private Date horario;
     private String categoria;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
