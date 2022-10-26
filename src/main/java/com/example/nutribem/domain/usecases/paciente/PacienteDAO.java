c149eed9cceda923e7688ca9950323ad81c0246d
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-10-26 19:52:44 -0300

    [FEAT] Criada interface PacienteDAO

---
+++ src/main/java/com/example/nutribem/domain/usecases/paciente/PacienteDAO.java
@@ -0,0 +1,12 @@
+package com.example.nutribem.domain.usecases.paciente;
+
+import com.example.nutribem.domain.entities.paciente.Paciente;
+import com.example.nutribem.domain.usecases.utils.DAO;
+
+import java.util.Optional;
+
+public interface PacienteDAO extends DAO<Paciente, Integer> {
+    Optional<Paciente> findByCpf(String cpf);
+
+    Optional<Paciente> findByNome(String nome);
+}
