6206da7125f8688f400b4bab2bdef18ae5e22c0c
IngridNery <neryingrid16@gmail.com>
2022-10-27 10:41:54 -0300

    [FEAT] Adiciona a interface PlanoNutricionalDAO

---
+++ src/main/java/com/example/nutribem/domain/usecases/planoNutricional/PlanoNutricionalDAO.java
@@ -0,0 +1,9 @@
+package com.example.nutribem.domain.usecases.planoNutricional;
+
+import com.example.nutribem.domain.entities.planoNutricional.PlanoNutricional;
+import com.example.nutribem.domain.usecases.utils.DAO;
+
+public interface PlanoNutricionalDAO extends DAO<PlanoNutricional,Integer> {
+
+
+}
