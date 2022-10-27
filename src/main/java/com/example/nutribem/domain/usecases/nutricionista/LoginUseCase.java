da262080a263d90b45a2cb2d9f8e9ea1ce97a60a
IngridNery <neryingrid16@gmail.com>
2022-10-27 10:05:35 -0300

    [FEAT] Cria o caso de uso LoginUseCase

---
+++ src/main/java/com/example/nutribem/domain/usecases/nutricionista/LoginUseCase.java
@@ -0,0 +1,35 @@
+package com.example.nutribem.domain.usecases.nutricionista;
+
+import com.example.nutribem.domain.usecases.utils.Notification;
+
+import static com.example.nutribem.domain.usecases.utils.Validator.nullOrEmpty;
+
+public class LoginUseCase {
+
+    private NutricionistaDAO dao;
+    Notification notification;
+  private static final int MINLENGTH = 4;
+
+    public Boolean login (String userName, String senha){
+        Notification notification = new Notification();
+
+
+        if(nullOrEmpty(senha)){
+            notification.addError("Senha is null or empty");
+        }
+        if(senha.length()<MINLENGTH){
+            notification.addError("Senha does not have four digits");
+        }
+        if(nullOrEmpty(userName)){
+            notification.addError("Nome de usuario is null or empty");
+        }
+
+        if(!dao.getSenhaFromUserName(userName).equals(senha)){
+            notification.addError("Senha invalida!");
+        }
+        if(notification.hasErrors())
+            throw new IllegalArgumentException(notification.errorMessage());
+        return true;
+
+    }
+}
