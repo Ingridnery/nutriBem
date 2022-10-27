d7b15869e614b1babaa6da8dec195778e561a241
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-10-27 02:33:17 -0300

    [FEAT] Cria o caso de uso UpdatePacienteUseCase

---
+++ src/main/java/com/example/nutribem/domain/usecases/paciente/UpdatePacienteUseCase.java
@@ -0,0 +1,25 @@
+package com.example.nutribem.domain.usecases.paciente;
+
+import com.example.nutribem.domain.entities.paciente.Paciente;
+import com.example.nutribem.domain.usecases.utils.Notification;
+import com.example.nutribem.domain.usecases.utils.Validator;
+
+import java.util.Optional;
+
+public class UpdatePacienteUseCase {
+    private PacienteDAO dao;
+
+    public UpdatePacienteUseCase(PacienteDAO dao) {
+        this.dao = dao;
+    }
+
+    public boolean update(Paciente paciente) {
+        Validator<Paciente> validator = new PacienteInputRequestValidator();
+        Notification notification = validator.validate(paciente);
+
+        if (notification.hasErrors())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        return dao.update(paciente);
+    }
+}
