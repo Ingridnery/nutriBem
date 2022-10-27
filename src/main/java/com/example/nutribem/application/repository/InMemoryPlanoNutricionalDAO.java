81d2ae9d96a88ceed1d2033f75f6254b96217bb4
IngridNery <neryingrid16@gmail.com>
2022-10-27 10:42:38 -0300

    [FEAT] Adiciona a classe InMemoryPlanoNutricionalDAO

---
+++ src/main/java/com/example/nutribem/application/repository/InMemoryPlanoNutricionalDAO.java
@@ -0,0 +1,56 @@
+package com.example.nutribem.application.repository;
+
+import com.example.nutribem.domain.entities.alimento.Alimento;
+import com.example.nutribem.domain.entities.planoNutricional.PlanoNutricional;
+import com.example.nutribem.domain.usecases.planoNutricional.PlanoNutricionalDAO;
+
+import java.util.*;
+
+public class InMemoryPlanoNutricionalDAO implements PlanoNutricionalDAO {
+    public static final Map<Integer, PlanoNutricional> db = new LinkedHashMap<>();
+    public static int idCounter = 0;
+
+
+    @Override
+    public Integer create(PlanoNutricional planoNutricional) {
+        planoNutricional.setId(++idCounter);
+        db.put(idCounter, planoNutricional);
+        return idCounter;
+    }
+
+    @Override
+    public Optional<PlanoNutricional> findOne(Integer key) {
+        if(db.containsKey(key))
+            return Optional.of(db.get(key));
+        return Optional.empty();
+    }
+
+    @Override
+    public List<PlanoNutricional> findAll() {
+        return new ArrayList<>(db.values());
+    }
+
+    @Override
+    public boolean update(PlanoNutricional planoNutricional) {
+        Integer id = planoNutricional.getId();
+        if(db.containsKey(id)){
+            db.replace(id, planoNutricional);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteByKey(Integer key) {
+        if(db.containsKey(key)){
+            db.remove(key);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean delete(PlanoNutricional planoNutricional) {
+        return deleteByKey(planoNutricional.getId());
+    }
+}
