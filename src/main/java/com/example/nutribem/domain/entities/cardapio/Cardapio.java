eed72dcfb8c87a432392a126cdf1eb6bc9f795ca
IngridNery <neryingrid16@gmail.com>
2022-10-31 08:56:19 -0300

    [REFACT] Adiciona referencia para o objeto Plano Nutricional

--- src/main/java/com/example/nutribem/domain/entities/cardapio/Cardapio.java
+++ src/main/java/com/example/nutribem/domain/entities/cardapio/Cardapio.java
@@ -1,18 +1,23 @@
 package com.example.nutribem.domain.entities.cardapio;
 
+import com.example.nutribem.domain.entities.planoNutricional.PlanoNutricional;
+
 public class Cardapio {
     private Integer id;
     private Integer numeroDia;
 
-    public Cardapio(Integer id, Integer numeroDia) {
+
+    private PlanoNutricional planoNutricional;
+
+    public Cardapio(Integer id, Integer numeroDia, PlanoNutricional planoNutricional) {
         this.id = id;
         this.numeroDia = numeroDia;
+        this.planoNutricional = planoNutricional;
     }
 
     public Integer getId() {
         return id;
     }
-
 
     public Integer getNumeroDia() {
         return numeroDia;
@@ -20,5 +25,13 @@ public class Cardapio {
 
     public void setNumeroDia(Integer numeroDia) {
         this.numeroDia = numeroDia;
+    }
+
+    public PlanoNutricional getPlanoNutricional() {
+        return planoNutricional;
+    }
+
+    public void setPlanoNutricional(PlanoNutricional planoNutricional) {
+        this.planoNutricional = planoNutricional;
     }
 }
