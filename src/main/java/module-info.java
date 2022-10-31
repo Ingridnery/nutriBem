9c3d4b632ef07fc37883605faa624f332e1bf79c
DiegoPagotto <diegodeabreupagotto@gmail.com>
2022-10-31 17:02:00 -0300

    [BUILD] Adiciona a lib OpenCSV ao module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.nutribem {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.opencsv;
 
     opens com.example.nutribem to javafx.fxml;
     exports com.example.nutribem;
