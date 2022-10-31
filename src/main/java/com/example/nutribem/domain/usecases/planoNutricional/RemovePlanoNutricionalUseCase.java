3bfd8bd4ec7b7199ccbc149827172582fe7f645a
IngridNery <neryingrid16@gmail.com>
2022-10-31 11:15:44 -0300

    [FEAT] Cria o caso de uso RemovePlanoNutricionalUseCase

---
+++ src/main/java/com/example/nutribem/domain/usecases/planoNutricional/RemovePlanoNutricionalUseCase.java
@@ -0,0 +1,24 @@
+package com.example.nutribem.domain.usecases.planoNutricional;
+
+
+import com.example.nutribem.domain.entities.planoNutricional.PlanoNutricional;
+import com.example.nutribem.domain.usecases.utils.EntityAlreadyExistsException;
+import com.example.nutribem.domain.usecases.utils.Notification;
+import com.example.nutribem.domain.usecases.utils.Validator;
+
+public class RemovePlanoNutricionalUseCase {
+
+    private PlanoNutricionalDAO planoNutricionalDAO;
+
+
+    public boolean remove (PlanoNutricional planoNutricional){
+        Validator<PlanoNutricional> validator = new PlanoNutricionalInputRequestValidator();
+        Notification notification = validator.validate(planoNutricional);
+
+        if(notification.hasErrors())
+            throw new IllegalArgumentException(notification.errorMessage());
+        if(planoNutricionalDAO.findOne(planoNutricional.getId()).isEmpty())
+            throw new EntityAlreadyExistsException("O plano nutricional: '"+ planoNutricional.getNome() +"' não está cadastrado.");
+        return planoNutricionalDAO.delete(planoNutricional);
+    }
+}
