b6ff26dc3fb8043a77680b5421cbdc46fbcfd4f2
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-11-01 00:32:48 -0300

    [FEAT] Criado caso de uso CreateRefeicaoUseCase

---
+++ src/main/java/com/example/nutribem/domain/usecases/refeicao/CreateRefeicaoUseCase.java
@@ -0,0 +1,23 @@
+package com.example.nutribem.domain.usecases.refeicao;
+
+import com.example.nutribem.domain.entities.refeicao.Refeicao;
+import com.example.nutribem.domain.usecases.utils.Notification;
+import com.example.nutribem.domain.usecases.utils.Validator;
+
+public class CreateRefeicaoUseCase {
+    private RefeicaoDAO dao;
+
+    public CreateRefeicaoUseCase(RefeicaoDAO dao) {
+        this.dao = dao;
+    }
+
+    public Integer insert(Refeicao refeicao) {
+        Validator<Refeicao> validator = new RefeicaoInputRequestValidator();
+        Notification notification = validator.validate(refeicao);
+
+        if (notification.hasErrors())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        return dao.create(refeicao);
+    }
+}
