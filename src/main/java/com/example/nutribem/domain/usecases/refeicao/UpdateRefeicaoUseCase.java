5a0f94da7abaebe54dac436a912ade1bda365b80
JeanCremonezzi <jeancremonezzi@hotmail.com>
2022-11-01 02:06:17 -0300

    [FEAT] Criado caso de uso UpdateRefeicaoUseCase

---
+++ src/main/java/com/example/nutribem/domain/usecases/refeicao/UpdateRefeicaoUseCase.java
@@ -0,0 +1,32 @@
+package com.example.nutribem.domain.usecases.refeicao;
+
+import com.example.nutribem.domain.entities.refeicao.Refeicao;
+import com.example.nutribem.domain.usecases.utils.EntityNotFoundException;
+import com.example.nutribem.domain.usecases.utils.Notification;
+import com.example.nutribem.domain.usecases.utils.Validator;
+
+public class UpdateRefeicaoUseCase {
+    private RefeicaoDAO dao;
+
+    public UpdateRefeicaoUseCase(RefeicaoDAO dao) {
+        this.dao = dao;
+    }
+
+    public boolean update(Refeicao refeicao) {
+        Validator<Refeicao> validator = new RefeicaoInputRequestValidator();
+        Notification notification = validator.validate(refeicao);
+
+        if (notification.hasErrors())
+            throw new IllegalArgumentException(notification.errorMessage());
+
+        Refeicao oldRefeicao = dao.findOne(refeicao.getId())
+                .orElseThrow(
+                    () -> new EntityNotFoundException("Refeicao nao encontrada.")
+                );
+
+        if (!refeicao.getCardapio().equals(oldRefeicao.getCardapio()))
+            throw new IllegalArgumentException("Cardapio da refeicao nao pode ser alterado.");
+
+        return dao.update(refeicao);
+    }
+}
