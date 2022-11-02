38ee5b70857c3e7d3aa40b4241ea0cb295a0d74c
IngridNery <neryingrid16@gmail.com>
2022-11-01 21:37:51 -0300

    [FEAT] Cria a classe Main

---
+++ src/main/java/com/example/nutribem/application/main/Main.java
@@ -0,0 +1,103 @@
+package com.example.nutribem.application.main;
+
+import com.example.nutribem.application.repository.*;
+import com.example.nutribem.domain.entities.alimento.Alimento;
+import com.example.nutribem.domain.usecases.alimento.*;
+import com.example.nutribem.domain.usecases.cardapio.*;
+import com.example.nutribem.domain.usecases.nutricionista.*;
+import com.example.nutribem.domain.usecases.paciente.*;
+import com.example.nutribem.domain.usecases.planoNutricional.*;
+import com.example.nutribem.domain.usecases.refeicao.*;
+
+import java.util.List;
+
+public class Main {
+
+    public static CreateAlimentoUseCase createAlimentoUseCase;
+    public static FindAlimentoUseCase findAlimentoUseCase;
+    public static RemoveAlimentoUseCase removeAlimentoUseCase;
+    public static UpdateAlimentoUseCase updateAlimentoUseCase;
+    public static ActivateAlimentoUseCase activateAlimentoUseCase;
+
+    public static CreateCardapioUseCase createCardapioUseCase;
+    public static UpdateCardapioUseCase updateCardapioUseCase;
+    public static FindCardapioUseCase findCardapioUseCase;
+    public static FindCardapioByNumeroDiaUseCase findCardapioByNumeroDiaUseCase;
+    public static RemoveCardapioUseCase removeCardapioUseCase;
+
+    public static CreateDicaSenhaUseCase createDicaSenhaUseCase;
+    public static CreateNutricionistaUseCase createNutricionistaUseCase;
+    public static LoginUseCase loginUseCase;
+    public static RecuperaSenhaUseCase recuperaSenhaUseCase;
+
+    public static ActivatePacienteUseCase activatePacienteUseCase;
+    public static CreatePacienteUseCase createPacienteUseCase;
+    public static FindPacienteUseCase findPacienteUseCase;
+    public static RemovePacienteUseCase removePacienteUseCase;
+    public static UpdatePacienteUseCase updatePacienteUseCase;
+
+    public static CreatePlanoNutricionalUseCase createPlanoNutricionalUseCase;
+    public static FindPlanoNutricionalByIdPacienteUseCase findPlanoNutricionalByIdPacienteUseCase;
+    public static FindPlanoNutricionalByNomeUseCase findPlanoNutricionalByNomeUseCase;
+    public static RemovePlanoNutricionalUseCase removePlanoNutricionalUseCase;
+    public static UpdatePlanoNutricionalUseCase updatePlanoNutricionalUseCase;
+
+    public static CreateRefeicaoUseCase createRefeicaoUseCase;
+    public static FindRefeicaoUseCase findRefeicaoUseCase;
+    public static RemoveRefeicaoUseCase removeRefeicaoUseCase;
+    public static UpdateRefeicaoUseCase updateRefeicaoUseCase;
+
+    public static void main(String[] args) {
+        configureInjection();
+        Alimento alimento = new Alimento("Batata",1,200,145,1, 4F, 15F,16F,17F);
+        createAlimentoUseCase.insert(alimento);
+        List<Alimento> alimentoList = findAlimentoUseCase.findAll();
+        for (Alimento alimento1: alimentoList) {
+            System.out.println(alimento1.getNome());
+        }
+
+    }
+    public static void configureInjection(){
+        AlimentoDAO alimentoDAO = new InMemoryAlimentoDAO();
+        createAlimentoUseCase = new CreateAlimentoUseCase(alimentoDAO);
+        findAlimentoUseCase = new FindAlimentoUseCase(alimentoDAO);
+        removeAlimentoUseCase = new RemoveAlimentoUseCase(alimentoDAO);
+        updateAlimentoUseCase = new UpdateAlimentoUseCase(alimentoDAO);
+        activateAlimentoUseCase = new ActivateAlimentoUseCase(alimentoDAO);
+
+        CardapioDAO cardapioDAO = new InMemoryCardapioDAO();
+        createCardapioUseCase = new CreateCardapioUseCase(cardapioDAO);
+        findCardapioUseCase = new FindCardapioUseCase(cardapioDAO);
+        removeCardapioUseCase = new RemoveCardapioUseCase(cardapioDAO);
+        findCardapioByNumeroDiaUseCase = new FindCardapioByNumeroDiaUseCase(cardapioDAO);
+        updateCardapioUseCase = new UpdateCardapioUseCase(cardapioDAO);
+
+        NutricionistaDAO nutricionistaDAO = new InMemoryNutricionistaDAO();
+        createDicaSenhaUseCase = new CreateDicaSenhaUseCase(nutricionistaDAO);
+        createNutricionistaUseCase = new CreateNutricionistaUseCase(nutricionistaDAO);
+        loginUseCase = new LoginUseCase(nutricionistaDAO);
+        recuperaSenhaUseCase = new RecuperaSenhaUseCase(nutricionistaDAO);
+
+        PacienteDAO pacienteDAO = new InMemoryPacienteDAO();
+        activatePacienteUseCase = new ActivatePacienteUseCase(pacienteDAO);
+        createPacienteUseCase = new CreatePacienteUseCase(pacienteDAO);
+        findPacienteUseCase = new FindPacienteUseCase(pacienteDAO);
+        removePacienteUseCase = new RemovePacienteUseCase(pacienteDAO);
+        updatePacienteUseCase = new UpdatePacienteUseCase(pacienteDAO);
+
+        PlanoNutricionalDAO planoNutricionalDAO = new InMemoryPlanoNutricionalDAO();
+        createPlanoNutricionalUseCase = new CreatePlanoNutricionalUseCase(planoNutricionalDAO);
+        findPlanoNutricionalByIdPacienteUseCase = new FindPlanoNutricionalByIdPacienteUseCase(planoNutricionalDAO);
+        findPlanoNutricionalByNomeUseCase = new FindPlanoNutricionalByNomeUseCase(planoNutricionalDAO);
+        removePlanoNutricionalUseCase = new RemovePlanoNutricionalUseCase();
+        updatePlanoNutricionalUseCase = new UpdatePlanoNutricionalUseCase(planoNutricionalDAO);
+
+        RefeicaoDAO refeicaoDAO = new InMemoryRefeicaoDAO();
+        createRefeicaoUseCase = new CreateRefeicaoUseCase(refeicaoDAO);
+        findRefeicaoUseCase = new FindRefeicaoUseCase(refeicaoDAO);
+        removeRefeicaoUseCase = new RemoveRefeicaoUseCase(refeicaoDAO);
+        updateRefeicaoUseCase = new UpdateRefeicaoUseCase(refeicaoDAO);
+
+
+    }
+}
