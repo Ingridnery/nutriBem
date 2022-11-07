5dd3d9c46a9f9a2fd715ee743f0ad36fea927810
DiegoPagotto <diegodeabreupagotto@gmail.com>
2022-11-07 14:53:19 -0300

    [FEAT] Cria a classe ValoresNutricionais

---
+++ src/main/java/com/example/nutribem/domain/usecases/valoresNutricionais/ValoresNutricionais.java
@@ -0,0 +1,90 @@
+package com.example.nutribem.domain.usecases.valoresNutricionais;
+
+public class ValoresNutricionais {
+    private Integer calorias;
+    private Integer colesterol;
+    private Boolean gluten;
+    private Float gordurasSaturadas;
+    private Float sodio;
+    private Float acucar;
+    private Float lactose;
+
+    public ValoresNutricionais(Integer calorias, Integer colesterol, Boolean gluten, Float gordurasSaturadas, Float sodio, Float acucar, Float lactose) {
+        this.calorias = calorias;
+        this.colesterol = colesterol;
+        this.gluten = gluten;
+        this.gordurasSaturadas = gordurasSaturadas;
+        this.sodio = sodio;
+        this.acucar = acucar;
+        this.lactose = lactose;
+    }
+
+    public ValoresNutricionais() {
+    }
+
+    public void somar(ValoresNutricionais novo){
+        this.calorias += novo.getCalorias();
+        this.colesterol += novo.getColesterol();
+        if(!this.gluten) this.gluten = novo.getGluten();
+        this.gordurasSaturadas += novo.getGordurasSaturadas();
+        this.sodio += novo.getSodio();
+        this.acucar += novo.getAcucar();
+        this.lactose += novo.getLactose();
+    }
+
+    public Integer getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(Integer calorias) {
+        this.calorias = calorias;
+    }
+
+    public Integer getColesterol() {
+        return colesterol;
+    }
+
+    public void setColesterol(Integer colesterol) {
+        this.colesterol = colesterol;
+    }
+
+    public Boolean getGluten() {
+        return gluten;
+    }
+
+    public void setGluten(Boolean gluten) {
+        this.gluten = gluten;
+    }
+
+    public Float getGordurasSaturadas() {
+        return gordurasSaturadas;
+    }
+
+    public void setGordurasSaturadas(Float gordurasSaturadas) {
+        this.gordurasSaturadas = gordurasSaturadas;
+    }
+
+    public Float getSodio() {
+        return sodio;
+    }
+
+    public void setSodio(Float sodio) {
+        this.sodio = sodio;
+    }
+
+    public Float getAcucar() {
+        return acucar;
+    }
+
+    public void setAcucar(Float acucar) {
+        this.acucar = acucar;
+    }
+
+    public Float getLactose() {
+        return lactose;
+    }
+
+    public void setLactose(Float lactose) {
+        this.lactose = lactose;
+    }
+}
