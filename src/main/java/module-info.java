be53785217f9056d9a4eead5c2af6d0d7326896c
IngridNery <neryingrid16@gmail.com>
2022-11-10 19:37:35 -0300

    [FEAT] Cria interface MainUI

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,5 +4,9 @@ module com.example.nutribem {
     requires com.opencsv;
 
     opens com.example.nutribem to javafx.fxml;
+    opens com.example.nutribem.domain.entities.paciente to javafx.fxml;
     exports com.example.nutribem;
+    exports com.example.nutribem.domain.entities.paciente;
+    exports com.example.nutribem.application.controller;
+    opens com.example.nutribem.application.controller to javafx.fxml;
 }
