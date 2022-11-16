98bdd755030eeb033f1e2c17c7c4c4ef4292c8de
IngridNery <neryingrid16@gmail.com>
2022-11-16 15:55:02 -0300

    [FEAT] Adiciona caminho para cardapio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,9 @@ module com.example.nutribem {
     opens com.example.nutribem.domain.entities.paciente to javafx.fxml;
     opens com.example.nutribem.domain.entities.alimento to javafx.fxml;
     opens com.example.nutribem.domain.entities.planoNutricional to javafx.fxml;
+    opens com.example.nutribem.domain.entities.cardapio to javafx.fxml;
     exports com.example.nutribem;
+    exports com.example.nutribem.domain.entities.cardapio;
     exports com.example.nutribem.domain.entities.planoNutricional;
     exports com.example.nutribem.domain.entities.alimento;
     exports com.example.nutribem.domain.entities.paciente;
