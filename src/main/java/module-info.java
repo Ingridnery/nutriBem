297a8b927a3a2db24d65f32c149d15ce690a04b7
IngridNery <neryingrid16@gmail.com>
2022-11-16 12:57:13 -0300

    [REFACT] Altera nome da classe HelloApplication para WindowLoader

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,11 @@ module com.example.nutribem {
 
     opens com.example.nutribem to javafx.fxml;
     opens com.example.nutribem.domain.entities.paciente to javafx.fxml;
+    opens com.example.nutribem.domain.entities.alimento to javafx.fxml;
+    opens com.example.nutribem.domain.entities.planoNutricional to javafx.fxml;
     exports com.example.nutribem;
+    exports com.example.nutribem.domain.entities.planoNutricional;
+    exports com.example.nutribem.domain.entities.alimento;
     exports com.example.nutribem.domain.entities.paciente;
     exports com.example.nutribem.application.controller;
     opens com.example.nutribem.application.controller to javafx.fxml;
