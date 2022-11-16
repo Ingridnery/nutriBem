e3143263519e932a142b86487447b1221f9e6d7e
IngridNery <neryingrid16@gmail.com>
2022-11-16 13:07:39 -0300

    [FEAT] Cria classe PlanoNutricionalUIController

---
+++ src/main/java/com/example/nutribem/application/controller/PlanoNutricionalUIController.java
@@ -0,0 +1,94 @@
+package com.example.nutribem.application.controller;
+
+import com.example.nutribem.WindowLoader;
+import com.example.nutribem.domain.entities.paciente.Paciente;
+import com.example.nutribem.domain.entities.planoNutricional.PlanoNutricional;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import static com.example.nutribem.application.main.Main.*;
+
+public class PlanoNutricionalUIController {
+
+    @FXML
+    private TextField txtName;
+    @FXML
+    private DatePicker dpStartDate;
+    @FXML
+    private DatePicker dpEndDate;
+    @FXML
+    private Button btnStatus;
+    @FXML
+    private Button btnConfirm;
+    @FXML
+    private Button btnCancel;
+
+    private PlanoNutricional planoNutricional;
+    private Paciente paciente;
+
+
+
+    public void backScene(ActionEvent actionEvent) throws IOException {
+        WindowLoader.setRoot("PlanoNutricionalManagementUI");
+
+        PlanoNutricionalManagementUIController controller = (PlanoNutricionalManagementUIController) WindowLoader.getController();
+        controller.setPlanoNutricional(paciente);
+
+
+
+
+    }
+
+    public void saveOrUpdate(ActionEvent actionEvent) throws IOException {
+        getEntityToView();
+        if(planoNutricional.getId() == null)
+            createPlanoNutricionalUseCase.insert(planoNutricional);
+        else
+            updatePlanoNutricionalUseCase.update(planoNutricional);
+        WindowLoader.setRoot("PlanoNutricionalManagementUI");
+        PlanoNutricionalManagementUIController controller = (PlanoNutricionalManagementUIController) WindowLoader.getController();
+        controller.setPlanoNutricional(paciente);
+    }
+
+    public void getEntityToView(){
+        if(planoNutricional==null)
+            planoNutricional= new PlanoNutricional();
+
+        planoNutricional.setNome(txtName.getText());
+        planoNutricional.setDataInicio(dpStartDate.getValue());
+        planoNutricional.setDataFim(dpEndDate.getValue());
+        planoNutricional.setPaciente(paciente);
+    }
+    public void setEntityIntoView(){
+        txtName.setText(planoNutricional.getNome());
+        dpStartDate.setValue(planoNutricional.getDataInicio());
+        dpEndDate.setValue(planoNutricional.getDataFim());
+    }
+
+    public void setPlanoNutricional(PlanoNutricional planoNutricional, UIMode mode){
+        Objects.requireNonNull(planoNutricional,"Plano nutricional não pode ser nulo!");
+
+        this.planoNutricional=planoNutricional;
+        setEntityIntoView();
+        if(mode == UIMode.VIEW)
+            configureViewMode();
+    }
+    public void setPaciente(Paciente paciente){
+       this.paciente=paciente;
+    }
+    private void configureViewMode(){
+        btnConfirm.setVisible(false);
+        btnCancel.setText("Fechar");
+
+
+        txtName.setDisable(true);
+        dpStartDate.setDisable(true);
+        dpEndDate.setDisable(true);
+    }
+}
