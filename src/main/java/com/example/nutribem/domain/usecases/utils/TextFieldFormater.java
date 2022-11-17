d80e6330e91c6d84f5ba07feb05f33a4b77fc36a
IngridNery <neryingrid16@gmail.com>
2022-11-17 17:09:04 -0300

    [FEAT] Cria classe TextFieldFormater

---
+++ src/main/java/com/example/nutribem/domain/usecases/utils/TextFieldFormater.java
@@ -0,0 +1,90 @@
+package com.example.nutribem.domain.usecases.utils;
+
+import javafx.scene.control.TextField;
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
+
+public class TextFieldFormater{
+
+    private final MaskFormatter mf = new MaskFormatter();
+    private TextField tf;
+    private String CaracteresValidos;
+    private String mask;
+
+    public TextFieldFormater() {
+    }
+
+    public void formatter(TextField tf, String CaracteresValidos, String mask) {
+        try {
+            mf.setMask(mask);
+        } catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        mf.setValidCharacters(CaracteresValidos);
+        mf.setValueContainsLiteralCharacters(false);
+        String text = tf.getText().replaceAll("[\\W]", "");
+
+        boolean repetir = true;
+        while (repetir) {
+
+            char ultimoCaractere;
+
+            if (text.equals("")) {
+                break;
+            } else {
+                ultimoCaractere = text.charAt(text.length() - 1);
+            }
+
+            try {
+                text = mf.valueToString(text);
+                repetir = false;
+            } catch (ParseException ex) {
+                text = text.replace(ultimoCaractere + "", "");
+                repetir = true;
+            }
+
+        }
+
+        tf.setText(text);
+
+        if (!text.equals("")) {
+            for (int i = 0; tf.getText().charAt(i) != ' ' && i < tf.getLength() - 1; i++) {
+                tf.forward();
+            }
+        }
+    }
+
+    public void formatter(){
+        formatter(this.tf, this.CaracteresValidos, this.mask);
+    }
+
+    public TextField getTf() {
+        return tf;
+    }
+
+    public void setTf(TextField tf) {
+        this.tf = tf;
+    }
+
+
+    public String getCaracteresValidos() {
+        return CaracteresValidos;
+    }
+
+
+    public void setCaracteresValidos(String CaracteresValidos) {
+        this.CaracteresValidos = CaracteresValidos;
+    }
+
+    public String getMask() {
+        return mask;
+    }
+
+    public void setMask(String mask) {
+        this.mask = mask;
+    }
+}
+
+
+
