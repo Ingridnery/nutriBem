8d594b47b6400f80ce9a1f7bdd87a4a305dd0684
IngridNery <neryingrid16@gmail.com>
2022-11-28 22:29:52 -0300

    [REFACT] Altera TextField para PasswordField na interface LoginUI

--- src/main/java/com/example/nutribem/application/controller/LoginUIController.java
+++ src/main/java/com/example/nutribem/application/controller/LoginUIController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -19,7 +20,7 @@ public class LoginUIController {
     @FXML
     private TextField txtUserName;
     @FXML
-    private TextField txtSenha;
+    private PasswordField txtSenha;
 
     private final AlertMessage alert = new AlertMessage();
 
